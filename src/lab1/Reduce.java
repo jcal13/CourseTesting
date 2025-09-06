944a63306679da670464a126bb1ace6c087fdad0
Jonathan Calver <calver@cs.toronto.edu>
2025-09-06 19:50:31 -0400

    adding Reduce

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class Reduce {
+
+    public static int reduce(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            }
+            else {
+                n -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
