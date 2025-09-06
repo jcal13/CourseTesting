b61bdd75a46884add968d9acf9fdb892a3d72bff
Jonathan Calver <calver@cs.toronto.edu>
2025-09-06 19:25:34 -0400

    adding Multiples

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Multiples {
+
+
+    public static int multiples() {
+        return multiples(1000, 3, 5);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int i = 1;
+        int count = 0;
+        while (i < n) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+}
